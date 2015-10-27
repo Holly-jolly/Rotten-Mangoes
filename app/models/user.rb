04fd56d1867a99ba0b52b2b5e97032420753157a
Holly-jolly <holly.haixin.l@gmail.com>
2015-10-27 20:42:10 +0000

    generate users controller and users/new view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email presence: true
+  validates :email, presence: true
   validates :password,length: { in: 6..20 }, on: :create
   validates :firstname, presence:true
   validates :lastname, presence:true
